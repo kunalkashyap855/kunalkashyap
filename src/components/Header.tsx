22ceb7ed0e2a7b579071ea0239c354e1ba24aa48
Kunal Kashyap <34695022+kunalkashyap855@users.noreply.github.com>
2023-04-09 01:56:21 -0400

    Landing Section 75%

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+
+
+function Header() {
+  return (
+    <div className="flex w-full justify-between">
+        <h2 className="font-extrabold text-gray-900 text-2xl">
+            Kunal Kashyap<span className="font-extrabold text-blue-500 text-4xl">.</span>
+        </h2>
+        <div className="text-lg">
+            <p className="font-semibold text-gray-900 hover:text-black">Projects</p>
+            <a href="https://kunalkashyap.substack.com/" target="_blank" className="font-semibold text-blue-500 hover:text-blue-700">Blog</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/sections/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
@@ -13,6 +14,11 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      'mobile': '320px',  // 320px and up
+      'tablet': '481px',  // 481px and up
+      'laptop': '769px',  // 769px and up
+    }
   },
   plugins: [],
 }
